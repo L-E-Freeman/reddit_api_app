9a477fc1b3da60bf68e5e1369a5a27758a563980
Laurence Freeman <laurence.freeman3@outlook.com>
2022-01-21 16:16:54 +0000

    Created models

--- saved_posts/models.py
+++ saved_posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class SavedPost(models.Model):
+    post_title = models.CharField(max_length=150)
+    number_upvotes = models.IntegerField()
+    number_comments = models.IntegerField()
+    post_link = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.post_title
+
+class TopLevelComment(models.Model):
+    parent_post = models.ForeignKey(
+        SavedPost, on_delete=models.CASCADE
+    )
+    contents = models.TextField()
+    number_upvotes = models.IntegerField()
