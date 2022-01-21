faa32fe139fc1c6ba468856b86566aee37a6603b
Laurence Freeman <laurence.freeman3@outlook.com>
2022-01-21 15:28:05 +0000

    Initial commit

---
+++ reddit_api_app/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('saved_posts/', include('saved_posts.urls')),
+    path('admin/', admin.site.urls),
+]
